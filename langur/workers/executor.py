16f1a40fd9b27f781fc11f0d6aaf04cbbf3a3c67
Anders Lie <anders.erik.lie@gmail.com>
2024-10-29 16:17:28 -0400

    exec works e2e tho hacky signal to start

--- langur/workers/executor.py
+++ langur/workers/executor.py
@@ -3,11 +3,13 @@ from langur.actions import ActionNode, ActionDefinitionNode
 from langur.baml_client.type_builder import TypeBuilder
 from langur.graph.graph import Graph
 from langur.graph.node import Node
-from langur.workers.worker import Worker
+from langur.workers.worker import STATE_DONE, Worker
 import langur.baml_client as baml
 
 
 class ExecutorWorker(Worker):
+    state: str = "WAITING"
+
     def get_frontier(self, graph: Graph) -> set[ActionNode]:
         '''
         Get the "frontier", i.e. unexecuted action nodes with only executed depedencies.
@@ -100,7 +102,7 @@ class ExecutorWorker(Worker):
         action_node.output = output
         return output
 
-    async def cycle(self, graph: Graph):
+    async def execute_frontier(self, graph: Graph):
         #action_nodes = graph.query_nodes_by_tag("action")
         frontier = self.get_frontier(graph)
 
@@ -109,4 +111,12 @@ class ExecutorWorker(Worker):
 
         await asyncio.gather(*[self.execute_node(graph, node) for node in frontier])
 
-        print("is done?", len(self.get_frontier(graph)) == 0)
+        is_done = len(self.get_frontier(graph)) == 0
+        #print("is done?", )
+        if is_done:
+            self.state = STATE_DONE
+    
+    async def cycle(self, graph: Graph):
+        # TODO super hacky, only works with exactly one executor and planner
+        if self.state == "WAITING" and len(graph.get_workers_with_state("WAITING")) == 1:
+            await self.execute_frontier(graph)
