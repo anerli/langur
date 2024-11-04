72add5b5fbe2359d17177aa1b3495e330bbd3c1e
Anders Lie <anders.erik.lie@gmail.com>
2024-11-04 04:09:33 -0500

    prime method compare example

--- challenges/grader/agent.py
+++ challenges/grader/agent.py
@@ -5,15 +5,12 @@ from langur.connectors import Workspace
 import shutil
 import pandas as pd
 
+
 EXPECTED_RESULT = pd.DataFrame({
     "First Name": ["Ethan", "Mia", "Ava", "Noah", "Bob"],
     "Last Name": ["Caldwell", "Kensington", "Harlow", "Winslow", "Smith"],
     "Points": [1, 4, 5, 5, 3]
 })
-
-def reset_workspace():
-    shutil.rmtree("./workspace", ignore_errors=True)
-    shutil.copytree("./template", "./workspace")
 
 def compare_results():
     actual_df = pd.read_csv("./workspace/grades.csv")
@@ -28,12 +25,10 @@ def compare_results():
     
     return are_equal
 
-if __name__ == "__main__":
-    os.chdir(os.path.dirname(__file__))
-    reset_workspace()
-
+def run():
     agent = Langur("Grade quizzes")
     agent.use(Workspace(path="./workspace"))
     agent.run()
 
     assert compare_results()
+
